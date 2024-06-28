aa8a19979e7d79f80cbd840becedbb017d6d13ab
electorn <daadiitya.007@gmail.com>
2024-06-28 12:40:20 +0530

    This is first commit with basic stuff and architecture of our project

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:tyccon/screens/login_screen.dart';
+import 'package:tyccon/utils/colors.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'tycoon',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: backgroundColor,
+      ),
+      routes: {
+      '/login':(context) => const LoginScreen(),
+      },
+      home: const LoginScreen(),
+    );
+  }
+}
---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:tyccon/widgets/custom_buttom.dart';
+import 'package:tyccon/widgets/custom_buttom.dart';
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'Start or join a meeting',
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: 20), // Adding some spacing between text and image
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 38.0),
+            child: Image.asset('assets/onboarding.jpg'),
+          ), // Adjust path as per// your project structure
+          CustomButton(text: 'Login'),
+       
+        ],
+      ),
+    );
+  }
+}
